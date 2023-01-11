0d50055e7ed185f37e5ed540760b3a731aca504a
Zachary Karpinski <zKarp@users.noreply.github.com>
2023-01-11 15:55:56 -0500

    updated tests

--- README.md
+++ README.md
@@ -2,6 +2,14 @@
  Deliberately insecure service
 
 
+## Known Vulnerable Libraries
+### demo-vc
+* junit:4.13
+* snakeyaml:1.27
+* gson:2.81
+* netty-all:4.1.0
+
+
 
 # Sources and references:
 TOML - https://galler.dev/keeping-dependencies-up-to-date-in-gradle-toml-files/
---
+++ demo-vc/src/test/java/com/zacharykarpinski/myapp/MyUnitTest.java
@@ -0,0 +1,16 @@
+package com.zacharykarpinski.myapp;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MyUnitTest {
+
+    @Test
+
+    public void SampleTestOne() {
+        final String NAME = "MyUnitTest";
+
+        assertEquals("MyUnitTest", NAME);
+
+    }
+}
