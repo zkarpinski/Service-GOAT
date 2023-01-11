353f24f05cd1c01545fea6175e47f6d836e33155
Zachary Karpinski <zKarp@users.noreply.github.com>
2023-01-11 15:22:04 -0500

    restructure

---
+++ demo-kotlin/build.gradle.kts
@@ -0,0 +1,32 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    kotlin("jvm") version "1.8.0"
+}
+
+group = "com.zacharykarpinski"
+version = "unspecified"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.1")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.1")
+    implementation("org.apache.commons:commons-text:1.9")
+    implementation(kotlin("stdlib-jdk8"))
+}
+
+tasks.getByName<Test>("test") {
+    useJUnitPlatform()
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
