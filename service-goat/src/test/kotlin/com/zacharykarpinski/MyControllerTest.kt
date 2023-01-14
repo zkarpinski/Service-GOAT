9c0ddaf9c537848b73f2c27ee72d6c3aebf7fe21
Zachary Karpinski <zKarp@users.noreply.github.com>
2023-01-14 16:15:40 -0500

    upgrade to micronaut 3.8.x

--- service-goat/src/test/kotlin/com/zacharykarpinski/MyControllerTest.kt
+++ service-goat/src/test/kotlin/com/zacharykarpinski/MyControllerTest.kt
@@ -1,18 +1,18 @@
 package com.zacharykarpinski
 
 import io.micronaut.http.HttpRequest
-import io.micronaut.http.client.RxHttpClient
+import io.micronaut.http.client.HttpClient
 import io.micronaut.http.client.annotation.Client
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import javax.inject.Inject
 
 @MicronautTest
 class MyControllerTest {
     @Inject
     @field:Client("/")
-    lateinit var client : RxHttpClient
+    lateinit var client : HttpClient
 
     @Test
     fun testHello() {
--- service-goat/src/test/kotlin/com/zacharykarpinski/ServiceGoatTest.kt
+++ service-goat/src/test/kotlin/com/zacharykarpinski/ServiceGoatTest.kt
@@ -1,9 +1,9 @@
 package com.zacharykarpinski
 import io.micronaut.runtime.EmbeddedApplication
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest
+import jakarta.inject.Inject
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import javax.inject.Inject
 
 @MicronautTest
 class ServiceGoatTest {
